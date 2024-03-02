59ac73bb5a4cb0849afdbdd91ef12e4cf9e7d09c
illia <fialkivskiji@gmail.com>
2024-03-03 00:17:13 +0200

    main nav

---
+++ src/components/MainNav.tsx
@@ -0,0 +1,9 @@
+const MainNav = () => {
+    return(
+        <button className="fond-bold hover:text-blue-500 hover:bg-white">
+            Log In
+        </button>
+    )
+}
+
+export default MainNav;
