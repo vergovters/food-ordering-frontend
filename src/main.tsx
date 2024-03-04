71ed9c871de67e9698b19e35d01fd8bdbd7c6a8e
illia <fialkivskiji@gmail.com>
2024-03-04 23:37:42 +0200

    fixing auth

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,24 @@ import './global.css'
 import { BrowserRouter } from 'react-router-dom'
 import AppRouters from "./AppRoutes"
 import Auth0ProviderNav from './auth/Auth0ProviderNav'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions:{
+    queries:{
+      refetchOnWindowFocus: false
+    }
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Auth0ProviderNav>
-        <AppRouters />
-      </Auth0ProviderNav>
+      <QueryClientProvider client={queryClient}>
+        <Auth0ProviderNav>
+          <AppRouters />
+        </Auth0ProviderNav>
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
