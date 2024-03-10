51e2b94da7265b81922d75c599849ddf16a352ae
illia <fialkivskiji@gmail.com>
2024-03-10 23:15:45 +0200

    callback page

--- src/auth/Auth0ProviderNav.tsx
+++ src/auth/Auth0ProviderNav.tsx
@@ -6,7 +6,7 @@ type Props = {
 }
 
 const Auth0ProviderNav = ({children}: Props) => {
-    const { createUser} = useCreateMyUser()
+  
     const domain = import.meta.env.VITE_AUTH0_DOMAIN;
     const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
     const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
@@ -16,9 +16,7 @@ const Auth0ProviderNav = ({children}: Props) => {
     }
 
     const onRedirectCallback = (appState?: AppState, user?: User) => {
-        if(user?.sub && user?.email){
-            createUser({auth0Id: user.sub, email: user.email})
-        }
+        
     }
 
     return(
---
+++ src/pages/AuthCallbackPage.tsx
@@ -0,0 +1,22 @@
+import { useCreateMyUser } from "@/api/UserApi";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+
+const AuthCallbackPage = () => {
+    const {user}  = useAuth0()
+    const { createUser} = useCreateMyUser()
+    const navigate = useNavigate()
+    const hasUser = useRef(false)
+
+    useEffect(() => {
+        if(user?.sub && user?.email && !hasUser.current){
+            createUser({auth0Id: user.sub, email: user.email})
+            hasUser.current = true
+        }
+        navigate("/")
+    }, [createUser, navigate, user])
+    return <>Loading...</>
+}
+
+export default AuthCallbackPage;
