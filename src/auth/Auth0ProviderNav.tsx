8853d660648d82e5273d4970f9466fc27aac66b1
illia <fialkivskiji@gmail.com>
2024-03-11 19:55:01 +0200

    auth jwt

--- src/auth/Auth0ProviderNav.tsx
+++ src/auth/Auth0ProviderNav.tsx
@@ -1,6 +1,5 @@
-import { useCreateMyUser } from "@/api/UserApi";
-import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
-import { Navigate, useNavigate } from "react-router-dom";
+import { Auth0Provider, User } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
     children: React.ReactNode
@@ -11,12 +10,13 @@ const Auth0ProviderNav = ({children}: Props) => {
     const domain = import.meta.env.VITE_AUTH0_DOMAIN;
     const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
     const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+    const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
-    if(!domain || !clientId || !redirectUri){
+    if(!domain || !clientId || !redirectUri || !audience){
         throw new Error("enable auth0")
     }
 
-    const onRedirectCallback = (appState?: AppState, user?: User) => {
+    const onRedirectCallback = () => {
         navigate("/auth-callback")
     }
 
@@ -26,6 +26,7 @@ const Auth0ProviderNav = ({children}: Props) => {
             clientId={clientId} 
             authorizationParams={{
                 redirect_uri: redirectUri,
+                audience,
             }}
             onRedirectCallback={onRedirectCallback}        
         >
