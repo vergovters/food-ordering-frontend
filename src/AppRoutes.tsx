c0488fb582cac3dcdc082e055c91684b1326cdd2
illia <fialkivskiji@gmail.com>
2024-03-15 20:02:51 +0200

    protected route

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -3,13 +3,16 @@ import Layot from "./layout/layot";
 import HomePage from "./pages/HomePage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
 import UserProfilePage from "./pages/UserProfilePage";
+import ProtectedRoute from "./auth/ProtectedRoute";
 
 export const AppRoutes = () => {
     return(
         <Routes>
             <Route path="/" element={<Layot showHero><HomePage/></Layot>}/>
             <Route path="/auth-callback" element={<AuthCallbackPage/>}/>
-            <Route path="/user-profile" element={<Layot><UserProfilePage/></Layot>}/>
+            <Route element={<ProtectedRoute/>}>
+                <Route path="/user-profile" element={<Layot><UserProfilePage/></Layot>}/>
+            </Route>
             <Route path="*" element={<Navigate to="/" />} />
         </Routes>
     )
