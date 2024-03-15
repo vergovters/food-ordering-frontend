1fef9a2dda8534ba9eb5ea1a874236fb17b70933
illia <fialkivskiji@gmail.com>
2024-03-15 19:32:24 +0200

    get user

--- src/api/UserApi.tsx
+++ src/api/UserApi.tsx
@@ -1,10 +1,46 @@
 
 import { useAuth0 } from "@auth0/auth0-react";
-import { useMutation } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import { toast } from "sonner";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
+export const useGetUser = () => {
+  const {getAccessTokenSilently} = useAuth0();
+
+  const getUserRequest = async ()=> {
+    const accessToken = await getAccessTokenSilently();
+
+    const response = await fetch(`${API_BASE_URL}/api/user`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if(!response.ok){
+      throw new Error("Failed to fetch user")
+    }
+
+    return response.json()
+  };
+
+  const {
+    data: currentUser, 
+    isLoading, 
+    error
+  } = useQuery("fetchCurrentUser", getUserRequest)
+
+  if(error){
+    toast.error(error.toString())
+  }
+
+  return { currentUser, isLoading };
+};
+
+
+//-------------------------------------------
 type CreateUserRequest = {
     auth0Id: string;
     email: string;
@@ -20,7 +56,6 @@ export const useCreateMyUser = () => {
         headers: {
           Authorization: `Bearer ${accessToken}`,
           "Content-Type": "application/json",
-        
         },
         body: JSON.stringify(user),
       });
--- src/pages/UserProfilePage.tsx
+++ src/pages/UserProfilePage.tsx
@@ -1,14 +1,20 @@
-import { useUpdateUser } from "@/api/UserApi";
+import { useGetUser, useUpdateUser } from "@/api/UserApi";
 import UserProfileForm from "@/components/forms/user-profile-form/UserProfileForm";
 
 
 const UserProfilePage = () => {
   const { updateUser, isLoading: isUpdateLoading } = useUpdateUser();
+  const { currentUser, isLoading: isGetLoading} = useGetUser();
+
+  if(isGetLoading){
+    return <span>Loading...</span>
+  }
 
     return (
         <UserProfileForm 
             onSave={updateUser}
             isLoading={isUpdateLoading}
+
         />
     );
   };
