cde2ecd4e99f812ee9a2ebcd17bbe29b7f420086
illia <fialkivskiji@gmail.com>
2024-03-19 21:42:53 +0200

    restaurant schema

--- src/components/forms/manage-restaurant-form/ManageRestaurantForm.tsx
+++ src/components/forms/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -1,8 +1,50 @@
+import { z } from "zod";
+
+const formSchema = z
+  .object({
+    restaurantName: z.string({
+      required_error: "restuarant name is required",
+    }),
+    city: z.string({
+      required_error: "city is required",
+    }),
+    country: z.string({
+      required_error: "country is required",
+    }),
+    deliveryPrice: z.coerce.number({
+      required_error: "delivery price is required",
+      invalid_type_error: "must be a valid number",
+    }),
+    estimatedDeliveryTime: z.coerce.number({
+      required_error: "estimated delivery time is required",
+      invalid_type_error: "must be a valid number",
+    }),
+    cuisines: z.array(z.string()).nonempty({
+      message: "please select at least one item",
+    }),
+    menuItems: z.array(
+      z.object({
+        name: z.string().min(1, "name is required"),
+        price: z.coerce.number().min(1, "price is required"),
+      })
+    ),
+    imageUrl: z.string().optional(),
+    imageFile: z.instanceof(File, { message: "image is required" }).optional(),
+  })
+  .refine((data) => data.imageUrl || data.imageFile, {
+    message: "Either image URL or image File must be provided",
+    path: ["imageFile"],
+  });
+
+type RestaurantFormData = z.infer<typeof formSchema>;
+
+
 type Props = {
+    onSave: (restaurantFormData: FormData) => void;
+    isLoading: boolean;
+  };
 
-}
-
-const ManageRestaurantForm = ({}: Props) => {
+const ManageRestaurantForm = ({ onSave, isLoading,}: Props) => {
 
 }
 
