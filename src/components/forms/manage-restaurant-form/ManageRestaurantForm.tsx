c6704d52a292c64c7e29fca836410a8b68dbd005
illia <fialkivskiji@gmail.com>
2024-03-19 21:54:46 +0200

    form template

--- src/components/forms/manage-restaurant-form/ManageRestaurantForm.tsx
+++ src/components/forms/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -1,3 +1,7 @@
+
+import { Form } from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import { z } from "zod";
 
 const formSchema = z
@@ -45,7 +49,56 @@ type Props = {
   };
 
 const ManageRestaurantForm = ({ onSave, isLoading,}: Props) => {
+    const form = useForm<RestaurantFormData>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+          cuisines: [],
+          menuItems: [{ name: "", price: 0 }],
+        },
+      });
 
+
+      
+  const onSubmit = (formDataJson: RestaurantFormData) => {
+    const formData = new FormData();
+
+    formData.append("restaurantName", formDataJson.restaurantName);
+    formData.append("city", formDataJson.city);
+    formData.append("country", formDataJson.country);
+
+    formData.append(
+      "deliveryPrice",
+      (formDataJson.deliveryPrice * 100).toString()
+    );
+    formData.append(
+      "estimatedDeliveryTime",
+      formDataJson.estimatedDeliveryTime.toString()
+    );
+    formDataJson.cuisines.forEach((cuisine, index) => {
+      formData.append(`cuisines[${index}]`, cuisine);
+    });
+    formDataJson.menuItems.forEach((menuItem, index) => {
+      formData.append(`menuItems[${index}][name]`, menuItem.name);
+      formData.append(
+        `menuItems[${index}][price]`,
+        (menuItem.price * 100).toString()
+      );
+    });
+
+    if (formDataJson.imageFile) {
+      formData.append(`imageFile`, formDataJson.imageFile);
+    }
+
+    onSave(formData);
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 bg-gray-50 p-10 rounded-lg"
+      ></form>
+    </Form>
 }
 
 export default ManageRestaurantForm;
