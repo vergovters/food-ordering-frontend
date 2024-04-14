c4e90d904de98b25c6056825d108c96009b7cd6c
illia <fialkivskiji@gmail.com>
2024-04-14 20:45:01 +0300

    search component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,14 @@
 import Comments from "@/components/comments/Comments/Comments";
+import SearchBar, { SearchForm } from "@/components/SearchBar";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+    const navigate = useNavigate()
+    const handleSearch = (searchValues: SearchForm) => {
+        navigate({
+            pathname: `/search/${searchValues.searchQuery}`
+        })
+    }
     return(
         <div className="flex flex-col gap-12">
             <div className="bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center mt-24">
@@ -8,6 +16,7 @@ const HomePage = () => {
                     The best choice for your food
                 </h1>
                 <span className="text-xl">Food is just waiting for you!</span>
+                <SearchBar placeHolder="Search" onSubmit={handleSearch}/>
             </div>
             <Comments/>
         </div>
