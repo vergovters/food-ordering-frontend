1c43bdf9e69d27b7f65a331a6af7c86bb1b15a98
illia <fialkivskiji@gmail.com>
2024-04-14 21:20:48 +0300

    SearchResultInfo

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,14 +1,76 @@
+import { useSearchRestaurants } from "@/api/ResApi";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
+import CuisineFilter from "@/components/CuisineFilter";
+import PaginationSelector from "@/components/PaginationSelector";
+import SearchBar, { SearchForm } from "@/components/SearchBar";
 
+
+export type SearchState = {
+    searchQuery: string;
+    page: number;
+    selectedCuisines: string[];
+    sortOption: string;
+  };
 
 const SearchPage = () => {
-    const {city} = useParams() 
+    const [searchState, setSearchState] = useState<SearchState>({
+        searchQuery: "",
+        page: 1,
+        selectedCuisines: [],
+        sortOption: "bestMatch",
+      });
 
-    return (
-        <div>
-            {city}
+    const {city} = useParams() 
+    const {results, isLoading} = useSearchRestaurants(searchState, city)
+
+    if (isLoading) {
+        <span>Loading ...</span>;
+      }
+    
+      if (!results?.data || !city) {
+        return <span>No results found</span>;
+      }
+    
+
+      return (
+        <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+          <div id="cuisines-list">
+            <CuisineFilter
+              selectedCuisines={searchState.selectedCuisines}
+              onChange={setSelectedCuisines}
+              isExpanded={isExpanded}
+              onExpandedClick={() =>
+                setIsExpanded((prevIsExpanded) => !prevIsExpanded)
+              }
+            />
+          </div>
+          <div id="main-content" className="flex flex-col gap-5">
+            <SearchBar
+              searchQuery={searchState.searchQuery}
+              onSubmit={setSearchQuery}
+              placeHolder="Search by Cuisine or Restaurant Name"
+              onReset={resetSearch}
+            />
+            <div className="flex justify-between flex-col gap-3 lg:flex-row">
+              <SearchResultInfo total={results.pagination.total} city={city} />
+              <SortOptionDropdown
+                sortOption={searchState.sortOption}
+                onChange={(value) => setSortOption(value)}
+              />
+            </div>
+    
+            {results.data.map((restaurant) => (
+              <SearchResultCard restaurant={restaurant} />
+            ))}
+            <PaginationSelector
+              page={results.pagination.page}
+              pages={results.pagination.pages}
+              onPageChange={setPage}
+            />
+          </div>
         </div>
-    );
-}
+      );
+    };
 
 export default SearchPage;
